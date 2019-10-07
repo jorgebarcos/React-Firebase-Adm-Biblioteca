d88a05bfc6e5a432057ae705ff8ce8bcce91e964
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-07 18:12:40 -0300

    Creando la barra principal

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import MostrarSuscriptor from './components/suscriptores/MostrarSuscriptor';
 import EditarSuscriptor from './components/suscriptores/EditarSuscriptor';
 import NuevoSuscriptor from './components/suscriptores/NuevoSuscriptor';
 
+import Navbar from './components/layout/Navbar';
+
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route exact path="/suscriptores" component={Suscriptores} />
-				<Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
-				<Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+			<Navbar />
+			<div className="container">
+				<Switch>
+					<Route exact path="/suscriptores" component={Suscriptores} />
+					<Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+					<Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
 
-				<Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
-			</Switch>
+					<Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+				</Switch>
+			</div>
 		</Router>
 	);
 }
