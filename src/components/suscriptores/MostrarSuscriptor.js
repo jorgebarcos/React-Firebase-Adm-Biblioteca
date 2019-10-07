fa0d4a96cccd1d475e4425c67151c978bca67220
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-07 18:00:15 -0300

    Agregando el Routing

---
+++ src/components/suscriptores/MostrarSuscriptor.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MostrarSuscriptor = () => {
+	return <h1>Mostrar Suscriptor</h1>;
+};
+
+export default MostrarSuscriptor;
---
+++ src/components/suscriptores/Suscriptores.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Suscriptores = () => {
+	return <h1>Suscriptores</h1>;
+};
+
+export default Suscriptores;
