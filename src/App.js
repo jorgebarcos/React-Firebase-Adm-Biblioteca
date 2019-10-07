5fe923fd0846ba45e0308bc09b7b9a0595ae5db9
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-07 19:18:19 -0300

    Conectando Firestore con React y Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 import Suscriptores from './components/suscriptores/Suscriptores';
 import MostrarSuscriptor from './components/suscriptores/MostrarSuscriptor';
@@ -10,18 +12,20 @@ import Navbar from './components/layout/Navbar';
 
 function App() {
 	return (
-		<Router>
-			<Navbar />
-			<div className="container">
-				<Switch>
-					<Route exact path="/suscriptores" component={Suscriptores} />
-					<Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
-					<Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<div className="container">
+					<Switch>
+						<Route exact path="/suscriptores" component={Suscriptores} />
+						<Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+						<Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
 
-					<Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
-				</Switch>
-			</div>
-		</Router>
+						<Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+					</Switch>
+				</div>
+			</Router>
+		</Provider>
 	);
 }
 
---
+++ src/store.js
@@ -0,0 +1,44 @@
+import { createStore, combineReducers, compose } from 'redux';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Configurar firestore.
+const firebaseConfig = {
+	apiKey: 'AIzaSyAX__d5fsWKwYkE_2UgqTb4gGkuVWZHIJk',
+	authDomain: 'bibliostore-cfa6f.firebaseapp.com',
+	databaseURL: 'https://bibliostore-cfa6f.firebaseio.com',
+	projectId: 'bibliostore-cfa6f',
+	storageBucket: 'bibliostore-cfa6f.appspot.com',
+	messagingSenderId: '1010486545862'
+};
+
+// Inicializar firebase
+firebase.initializeApp(firebaseConfig);
+
+// configuracion de react-redux
+const rrfConfig = {
+	userProfile: 'users',
+	useFirestoreForProfile: true
+};
+
+// crear el enhacer con compose de redux y firestore
+const createStoreWithFirebase = compose(reactReduxFirebase(firebase, rrfConfig), reduxFirestore(firebase))(createStore);
+
+// Reducers
+const rootReducer = combineReducers({
+	firebase: firebaseReducer,
+	firestore: firestoreReducer
+});
+
+// state inicial
+const initialState = {};
+
+// Create el store
+const store = createStoreWithFirebase(
+	rootReducer,
+	initialState,
+	compose(reactReduxFirebase(firebase), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
+export default store;
