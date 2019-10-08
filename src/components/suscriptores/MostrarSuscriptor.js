c166249d5ca18d8d84d013d87db441f047eaf585
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-08 18:22:41 -0300

    Mostrando un Suscriptor

--- src/components/suscriptores/MostrarSuscriptor.js
+++ src/components/suscriptores/MostrarSuscriptor.js
@@ -6,21 +6,51 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 
-const MostrarSuscriptor = () => {
+const MostrarSuscriptor = ({ suscriptor }) => {
+	if (!suscriptor) return <Spinner />;
 	return (
+		<div className="row">
+			<div className="col-md-6 mb-4">
+				<Link to="/suscriptores" className="btn btn-secondary">
+					<i className="fas fa-arrow-circle-left" /> {''} Volver al Listado
+				</Link>
+			</div>
+			<div className="col-md-6">
+				<Link to={`/suscriptores/editar/${suscriptor.id}`} className="btn btn-primary float-right">
+					<i className="fas fa-pencil-alt" /> {''} Editar Suscriptor
+				</Link>
+			</div>
+			<hr className="mx-5 w-100" />
+			<div className="col-12">
+				<h2 className="mb-4">
+					{suscriptor.nombre} {suscriptor.apellido}
+				</h2>
 
-	)
+				<p>
+					<span className="font-weight-bold">Carrera:</span> {''}
+					{suscriptor.carrera}
+				</p>
+				<p>
+					<span className="font-weight-bold">Código:</span> {''}
+					{suscriptor.codigo}
+				</p>
+			</div>
+		</div>
+	);
+};
+MostrarSuscriptor.propTypes = {
+	firestore: PropTypes.object.isRequired
 };
 
 export default compose(
-	firestoreConnect(props => [
+	firestoreConnect((props) => [
 		{
-			collection : 'suscriptores', 
-			storeAs : 'suscriptor',
-			doc : props.match.params.id
+			collection: 'suscriptores',
+			storeAs: 'suscriptor',
+			doc: props.match.params.id
 		}
 	]),
-	connect(({firestore: {ordered}}, props) => ({
-		suscriptor : ordered.suscriptor && ordered.suscriptor[0]
+	connect(({ firestore: { ordered } }, props) => ({
+		suscriptor: ordered.suscriptor && ordered.suscriptor[0]
 	}))
-)(MostrarSuscriptor)
+)(MostrarSuscriptor);
