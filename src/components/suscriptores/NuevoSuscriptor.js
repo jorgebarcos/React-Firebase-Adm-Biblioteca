5ad8751098053857113fe1281b5efd9c2ee12748
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-08 16:20:36 -0300

    Creando el Formulario para Nuevos Suscriptores

--- src/components/suscriptores/NuevoSuscriptor.js
+++ src/components/suscriptores/NuevoSuscriptor.js
@@ -1,9 +1,87 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NuevoSuscriptor extends Component {
-	state = {};
+	state = {
+		nombre: '',
+		apellido: '',
+		carrera: '',
+		codigo: ''
+	};
 	render() {
-		return <h1>Nuevo Suscriptor</h1>;
+		return (
+			<div className="row">
+				<div className="col-12 mb-4">
+					<Link to={'/suscriptores'} className="btn btn-secondary">
+						<i className="fas fa-arrow-circle-left" /> {''} Volver al Listado
+					</Link>
+				</div>
+				<div className="col-12">
+					<h2>
+						<i className="fas fa-user-plus" /> {''} Nuevo Suscriptor
+					</h2>
+					<div className="row justify-content-center">
+						<div className="col-md-8 mt-5">
+							<form>
+								<div className="form-group">
+									<label>Nombre:</label>
+									<input
+										type="text"
+										className="form-control"
+										name="nombre"
+										placeholder="Nombre del Suscriptor"
+										required
+										onChange={this.leerDato}
+										value={this.state.nombre}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label>Apellido:</label>
+									<input
+										type="text"
+										className="form-control"
+										name="apellido"
+										placeholder="Apellido del Suscriptor"
+										required
+										onChange={this.leerDato}
+										value={this.state.apellido}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label>Carrera:</label>
+									<input
+										type="text"
+										className="form-control"
+										name="carrera"
+										placeholder="Carrera del Suscriptor"
+										required
+										onChange={this.leerDato}
+										value={this.state.carrera}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label>Codigo:</label>
+									<input
+										type="text"
+										className="form-control"
+										name="codigo"
+										placeholder="Codigo del Suscriptor"
+										required
+										onChange={this.leerDato}
+										value={this.state.codigo}
+									/>
+								</div>
+
+								<input type="submit" value="Agregar Suscriptor" className="btn btn-success" />
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
