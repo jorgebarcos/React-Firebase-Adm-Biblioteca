96b6e5b4f769b3a053b4baaad39edd3503d9ea13
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-08 16:44:21 -0300

    Seleccionando un Suscriptor

--- src/components/suscriptores/MostrarSuscriptor.js
+++ src/components/suscriptores/MostrarSuscriptor.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
 
 const MostrarSuscriptor = () => {
-	return <h1>Mostrar Suscriptor</h1>;
+	return (
+
+	)
 };
 
-export default MostrarSuscriptor;
+export default compose(
+	firestoreConnect(props => [
+		{
+			collection : 'suscriptores', 
+			storeAs : 'suscriptor',
+			doc : props.match.params.id
+		}
+	]),
+	connect(({firestore: {ordered}}, props) => ({
+		suscriptor : ordered.suscriptor && ordered.suscriptor[0]
+	}))
+)(MostrarSuscriptor)
