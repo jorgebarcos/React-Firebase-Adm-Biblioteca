aca0f5a56cf7f169ddeeff9d6e2abcc08d192b32
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-09 18:14:14 -0300

    Creando el Componente para el Prestamo y agregando información

--- src/components/libros/PrestamoLibro.js
+++ src/components/libros/PrestamoLibro.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
 
 class PrestamoLibro extends Component {
 	state = {};
 	render() {
-		return <h1>Prestamo Libro</h1>;
+		// Extraer el libro
+		const { libro } = this.props;
+
+		// mostrar el spinner
+		if (!libro) return <Spinner />;
+
+		return (
+			<div className="row">
+				<div className="row">
+					<div className="col-12 mb-4">
+						<Link to={'/'} className="btn btn-secondary">
+							<i className="fas fa-arrow-circle-left" /> {''} Volver al Listado
+						</Link>
+					</div>
+					<div className="col-12">
+						<h2>
+							<i className="fas fa-book" /> {''} Solicitar Prestamo : {libro.titulo}
+						</h2>
+
+						<div className="row justify-content-center mt-5">
+							<div className="col-md-8">
+								<form>
+									<legend className="color-primary text-center">
+										Busca el Suscriptor por Código
+									</legend>
+
+									<div className="form-group">
+										<input
+											type="text"
+											name="busqueda"
+											className="form-control"
+											onChange={this.leerDato}
+										/>
+									</div>
+									<input type="submit" value="Buscar Alumno" className="btn btn-success btn-block" />
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
-export default PrestamoLibro;
+PrestamoLibro.propTypes = {
+	firestore: PropTypes.object.isRequired
+};
+
+export default compose(
+	firestoreConnect((props) => [
+		{
+			collection: 'libros',
+			storeAs: 'libro',
+			doc: props.match.params.id
+		}
+	]),
+	connect(({ firestore: { ordered } }, props) => ({
+		libro: ordered.libro && ordered.libro[0]
+	}))
+)(PrestamoLibro);
